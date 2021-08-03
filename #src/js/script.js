42cfd0c8b4cebaff570fd7a37ac27a352e9fdb5f
andrey678 <drew6496@yandex.ru>
2021-08-03 14:53:14 +0300

    Add menu 'burger'. Add portrait mode 768px for header menu.Fix the menu to the top.

--- #src/js/script.js
+++ #src/js/script.js
@@ -17,3 +17,14 @@ document.querySelector('body').classList.add('no-webp');
 });
 
 
+// МЕНЮ БУРГЕР
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	$('.header__link').click(function(event){
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
